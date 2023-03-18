2babd972d401ce99585459d9ec3143e156453773
babySushi1212 <public92817@gmail.com>
2023-03-18 22:09:36 +0800

    feat: update reservation api

--- src/main/java/webapp/booking/controller/ReservationController.java
+++ src/main/java/webapp/booking/controller/ReservationController.java
@@ -3,9 +3,13 @@ package webapp.booking.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import webapp.booking.dto.OpenHourDTO;
+import webapp.booking.pojo.OpenHour;
 import webapp.booking.service.ReservationService;
 
 @RestController
@@ -16,7 +20,33 @@ public class ReservationController {
     private ReservationService reservationService;
 
     @GetMapping("/ls")
-    public List<OpenHourDTO> getAllOpenHour(){
+    public List<OpenHourDTO> getAllOpenHour() {
         return reservationService.getAllDTO();
+    }
+
+    @GetMapping("/insert-test")
+//    public void insert(@RequestParam OpenHourDTO dto){
+    public void insert(@RequestParam(name = "week") String week) {
+//        reservationService.insert(dto);
+    }
+
+    @GetMapping("/insert")
+    public void insert(@RequestParam OpenHourDTO dto) {
+        reservationService.insert(dto);
+    }
+
+    @PostMapping("/insert-all")
+    public void insertAll(@RequestBody List<OpenHourDTO> dtoList) {
+        reservationService.insertAll(dtoList);
+    }
+
+    @GetMapping("/week")
+    public OpenHourDTO selectByWeek(@RequestParam Integer week){
+        return reservationService.selectByWeek(week);
+    }
+
+    @PostMapping("/updateByWeek")
+    public void updateByWeek(@RequestBody OpenHourDTO dto){
+        reservationService.updateByWeek(dto);
     }
 }
--- src/main/java/webapp/booking/service/ReservationService.java
+++ src/main/java/webapp/booking/service/ReservationService.java
@@ -1,6 +1,9 @@
 package webapp.booking.service;
 
 import core.BasicService;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
@@ -14,7 +17,48 @@ import webapp.booking.repository.OpenHourRepository;
 @Service
 public class ReservationService extends BasicService<OpenHourRepository, OpenHour, OpenHourDTO> {
 
+    // Spring recommend constructor injection
     public ReservationService(ModelMapper modelMapper) {
         super(modelMapper);
+    }
+
+    @Autowired
+    OpenHourRepository openHourRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+
+    public void updateByWeek(OpenHourDTO dto) {
+        System.out.println("into service");
+        OpenHour openHour = openHourRepository.findByWeek(dto.getWeek());
+        openHour.setOpenTimeStart(dto.getOpenTimeStart());
+        openHour.setOpenTimeEnd(dto.getOpenTimeEnd());
+        openHourRepository.save(openHour);
+    }
+
+    public OpenHourDTO selectByWeek(Integer week) {
+        return modelMapper.map(openHourRepository
+            .findByWeek(week), OpenHourDTO.class);
+    }
+
+    public void insert(OpenHourDTO dto) {
+        modelMapper.getConfiguration()
+            .setMatchingStrategy(MatchingStrategies.LOOSE);
+        openHourRepository.save(modelMapper.map(dto, OpenHour.class));
+    }
+
+    public void insertAll(List<OpenHourDTO> dto) {
+        List<OpenHour> insertList = dto
+            .stream()
+            .map(this::dtoToEntity)
+            .collect(Collectors.toList());
+        openHourRepository.saveAll(insertList);
+    }
+
+    private OpenHour dtoToEntity(OpenHourDTO pojo) {
+        modelMapper.getConfiguration()
+            .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper.map(pojo, OpenHour.class);
     }
 }

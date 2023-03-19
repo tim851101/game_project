89e3797e4c2c6b9f7581e3df1251d11314cac725
babySushi1212 <public92817@gmail.com>
2023-03-19 20:03:09 +0800

    refactor: handle warrnings

--- src/main/java/webapp/booking/repository/OpenHourRepository.java
+++ src/main/java/webapp/booking/repository/OpenHourRepository.java
@@ -1,6 +1,5 @@
 package webapp.booking.repository;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import webapp.booking.pojo.OpenHour;

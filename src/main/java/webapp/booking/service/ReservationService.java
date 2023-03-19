43a52f53b252158192818cff936928cb78cd1851
babySushi1212 <public92817@gmail.com>
2023-03-19 20:03:09 +0800

    refactor: handle warrnings

--- src/main/java/webapp/booking/service/ReservationService.java
+++ src/main/java/webapp/booking/service/ReservationService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import webapp.booking.dto.OpenHourDTO;
 import webapp.booking.pojo.OpenHour;

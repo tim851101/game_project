16256b696959b48e016b47bbf7e69e0021d21771
a81194 <a81194@icloud.com>
2023-03-20 09:44:26 +0800

    feat: 會員註冊

---
+++ src/main/java/webapp/member/controller/LoginController.java
@@ -0,0 +1,22 @@
+package webapp.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class LoginController {
+
+//    @GetMapping("/foreground/login")
+//    public String showLoginPage() {
+//        return "foreground/login"; //返回視圖名稱
+//    }
+//    @GetMapping("/")
+//    @Qualifier("loginController")
+//    public String showLoginPage() {
+//        return "login";
+//    }
+
+
+}
--- src/main/java/webapp/member/pojo/Members.java
+++ src/main/java/webapp/member/pojo/Members.java
@@ -1,10 +1,7 @@
 package webapp.member.pojo;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,46 +15,47 @@ import java.util.Date;
 @Table(name = "MEMBERS")
 public class Members {
     @Id
-    @Column(name = "MEM_NO")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEM_NO",insertable = false)
     private Integer memNo;
 
-    @Column(name = "MEM_NAME")
+    @Column(name = "MEM_NAME" ,nullable = false, length = 10)
     private String memName;
 
-    @Column(name = "MEM_GENDER")
+    @Column(name = "MEM_GENDER", nullable = false)
     private Byte memGender;
 
     @Column(name = "MEM_PIC")
     private byte[] memPic;
 
-    @Column(name = "MEM_EMAIL")
+    @Column(name = "MEM_EMAIL", nullable = false, length = 50, unique = true)
     private String memEmail;
 
-    @Column(name = "MEM_PASSWORD")
+    @Column(name = "MEM_PASSWORD", nullable = false, length = 40)
     private String memPassword;
 
-    @Column(name = "MEM_PHONE")
+    @Column(name = "MEM_PHONE", length = 10)
     private String memPhone;
 
-    @Column(name = "MEM_ADDRESS")
+    @Column(name = "MEM_ADDRESS", nullable = false, length = 100)
     private String memAddress;
 
     @Column(name = "MEM_BIRTHDAY")
     private Date memBirthday;
 
-    @Column(name = "COUPON")
+    @Column(name = "COUPON",insertable = false)
     private Integer coupon;
 
-    @Column(name = "RESERVE_AUTH")
+    @Column(name = "RESERVE_AUTH",insertable = false)
     private Boolean reserveAuth;
 
-    @Column(name = "MEM_VIO")
+    @Column(name = "MEM_VIO",insertable = false)
     private Integer memVIO;
 
     @Column(name = "MEM_STATUS")
     private Date memStatus;
 
 
-
-
+    public Members(String memName, Byte memGender, byte[] memPic, String memEmail, String password, String memPhone, String memAddress, Date memBirthday, String encode) {
+    }
 }

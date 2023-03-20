4019f52be4dbc25e113304adedc7ae6ee315434a
babySushi1212 <public92817@gmail.com>
2023-03-20 10:55:46 +0800

    refactor: delete redundant CRUD in controller

--- src/main/java/webapp/booking/controller/ReservationController.java
+++ src/main/java/webapp/booking/controller/ReservationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import webapp.booking.dto.OpenHourDTO;
 import webapp.booking.service.ReservationService;
@@ -25,29 +25,15 @@ public class ReservationController {
         return reservationService.getAllDTO();
     }
 
-    @GetMapping("/insert-test")
-//    public void insert(@RequestParam OpenHourDTO dto){
-    public void insert(@RequestParam(name = "week") String week) {
-//        reservationService.insert(dto);
-    }
-
-    @GetMapping("/insert")
-    public void insert(@RequestParam OpenHourDTO dto) {
-        reservationService.insert(dto);
-    }
-
-    @PostMapping("/insert-all")
-    public void insertAll(@RequestBody List<OpenHourDTO> dtoList) {
-        reservationService.insertAll(dtoList);
-    }
-
-    @GetMapping("/week")
-    public OpenHourDTO selectByWeek(@RequestParam Integer week){
-        return reservationService.selectByWeek(week);
-    }
-
-    @PostMapping("/updateByWeek")
-    public void updateByWeek(@RequestBody OpenHourDTO dto){
-        reservationService.updateByWeek(dto);
+    @PostMapping("/save-all")
+    @ResponseBody
+    public Boolean insertAll(@RequestBody List<OpenHourDTO> dtoList) {
+        try {
+            reservationService.insertAllDTO(dtoList);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }

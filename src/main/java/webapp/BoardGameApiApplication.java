368cb781f0e22f615ab28e39dafe4ffb1c4879f1
babySushi1212 <public92817@gmail.com>
2023-03-20 10:54:07 +0800

    feat: add CommandLineRunner to main method

--- src/main/java/webapp/BoardGameApiApplication.java
+++ src/main/java/webapp/BoardGameApiApplication.java
@@ -1,13 +1,8 @@
 package webapp;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-import webapp.booking.pojo.OpenHour;
-import webapp.booking.repository.OpenHourRepository;
 
 //@Configuration
 //@EnableAutoConfiguration
@@ -15,20 +10,14 @@ import webapp.booking.repository.OpenHourRepository;
 @SpringBootApplication
 public class BoardGameApiApplication implements CommandLineRunner {
 
-    @Autowired
-    private OpenHourRepository openHourRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(BoardGameApiApplication.class, args);
+        // SpringApplication.exit(SpringApplication.run(BoardGameApiApplication.class, args));
+
     }
 
     @Override
     public void run(String... args) throws Exception {
-        // Testing the findByWeek method
-        Integer week = 3;
-        OpenHour openHour = openHourRepository.findByWeek(week);
-        System.out.println("Open hour for week " + week + ": " + openHour);
-//        SpringApplication.exit(SpringApplication.run(BoardGameApiApplication.class, args));
     }
 }
 

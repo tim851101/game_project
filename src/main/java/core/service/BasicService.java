e4b63c0ec3969b254798423acae531ffd1e9ddfd
babySushi1212 <public92817@gmail.com>
2023-03-20 10:53:06 +0800

    feat: add insert all to BasicService

--- src/main/java/core/service/BasicService.java
+++ src/main/java/core/service/BasicService.java
@@ -4,7 +4,6 @@ import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +15,8 @@ import org.springframework.stereotype.Service;
  * @param <D> DTO
  */
 @Service
-public class BasicService<R extends JpaRepository<E, Integer>, E, D> implements intrinsicService<D> {
+public class BasicService<R extends JpaRepository<E, Integer>, E, D>
+    implements intrinsicService<D> {
 
     protected final R repository;
     private final ModelMapper modelMapper;
@@ -33,8 +33,6 @@ public class BasicService<R extends JpaRepository<E, Integer>, E, D> implements 
      * @return List<D> D is type of DTO
      */
     public List<D> getAllDTO() {
-        modelMapper.getConfiguration()
-            .setMatchingStrategy(MatchingStrategies.LOOSE);
         return repository.findAll()
             .stream()
             .map(this::entityToDTO)
@@ -48,11 +46,23 @@ public class BasicService<R extends JpaRepository<E, Integer>, E, D> implements 
      * @return D type of DTO
      */
     private D entityToDTO(E entity) {
-        modelMapper.getConfiguration()
-            .setMatchingStrategy(MatchingStrategies.LOOSE);
         // get runtime generic class type
         return modelMapper.map(entity, (Class<D>) ((ParameterizedType) getClass()
             .getGenericSuperclass())
             .getActualTypeArguments()[2]);
+    }
+
+    public void insertAllDTO(List<D> dto) {
+        List<E> insertList = dto
+            .stream()
+            .map(this::dtoToEntity)
+            .collect(Collectors.toList());
+        repository.saveAll(insertList);
+    }
+
+    private E dtoToEntity(D pojo) {
+        return modelMapper.map(pojo, (Class<E>) ((ParameterizedType) getClass()
+            .getGenericSuperclass())
+            .getActualTypeArguments()[1]);
     }
 }

209f0ba87a8ccac9acf96612f6d07714906e3108
styleore <tim851101@gmail.com>
2023-04-06 15:03:15 +0800

    feat 顯示 圖片

--- src/main/java/webapp/product/controller/ProductPicController.java
+++ src/main/java/webapp/product/controller/ProductPicController.java
@@ -1,12 +1,15 @@
 package webapp.product.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import webapp.product.pojo.ProductPic;
 import webapp.product.repository.ProductPicRepository;
 import javax.sql.rowset.serial.SerialBlob;
-import java.io.IOException;
+import java.io.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -37,5 +40,42 @@ public class ProductPicController {
 
         return ResponseEntity.ok("圖片存進資料庫成功");
     }
+    @GetMapping("/getimage" )
+    public ResponseEntity<byte[]> getimgage(Integer picno) throws SQLException, IOException {
+        try {
 
+        ProductPic productPic=productPicRepository.getReferenceById(picno);
+        InputStream inputStream=productPic.getPdPic().getBinaryStream();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[4096]; // 4KB buffer size
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            byte[] bytes = outputStream.toByteArray();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
+        }catch (Exception e){
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            String imgFile ="/static/foregronud/static/image/none.jpg";
+            InputStream inputStream=new FileInputStream(imgFile);;
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            byte[] bytes = outputStream.toByteArray();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
+        }
+
+
+    }
 }

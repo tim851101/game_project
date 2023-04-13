633ae9eb9da36e27248402e95ffd8fab2bd00a98
babySushi1212 <public92817@gmail.com>
2023-04-13 18:40:52 +0800

    refactor: modify RedisConfig

--- src/main/java/core/config/RedisConfig.java
+++ src/main/java/core/config/RedisConfig.java
@@ -1,2 +1,49 @@
-package core.config;public class RedisConfig {
+package core.config;
+
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        config.setDatabase(5);
+        config.setPassword("");
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(15);
+        poolConfig.setMaxIdle(8);
+        poolConfig.setMinIdle(0);
+        poolConfig.setMaxWait(Duration.ofSeconds(5));
+
+        JedisConnectionFactory factory = new JedisConnectionFactory(config);
+        factory.setPoolConfig(poolConfig); // TODO: deprecated
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Map<String, List<Integer>>> seatTemplate() {
+        RedisTemplate<String, Map<String, List<Integer>>> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(List.class));
+        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(List.class));
+        return template;
+    }
 }

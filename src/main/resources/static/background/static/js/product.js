cef43f7d5a9ef4c36388438c2c9a38306b30ed64
Maswell <ases51101@gmail.com>
2023-04-16 22:30:08 +0800

    後台商品管理套入全部查詢按鈕與單筆查詢功能

--- src/main/resources/static/background/static/js/product.js
+++ src/main/resources/static/background/static/js/product.js
@@ -36,57 +36,73 @@ $('#product-form').submit(e => {
     });
 
 //  單筆查詢
-window.onload = function() {
- const findByIdBtn = document.getElementById('findById');
-     const inputEmployeeId = document.getElementById('productId');
-     const showTable = document.getElementById('show');
+$(document).ready(function() {
+  const findByIdBtn = document.getElementById('findById');
+  const inputProductId = document.getElementById('productId');
+  const allShowTable = document.getElementById('allshow').getElementsByTagName('tbody')[0];
+  const paginationContainer = document.querySelector('.pagination');
 
-     findByIdBtn.addEventListener('click', () => {
-         const productId = inputEmployeeId.value;
+  findByIdBtn.addEventListener('click', () => {
+    const productId = inputProductId.value;
 
-         fetch(`/product/find-one?id=${productId}`) //response
-             .then(response => response.json()   //promise 轉 data
-             )
-             .then(data => {
-                 console.log(`/emp/find-one?id='${productId}'`);
-                 console.log(data);
-                 showTable.setAttribute('border', '1');
+    fetch(`/product/find-one?id=${productId}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(`/product/find-one?id='${productId}'`);
+        console.log(data);
 
-                 const table = document.getElementById('show');
-                 table.innerHTML="";
-                 for (const property in data) {
-                     const row = table.insertRow();
-                     const keyCell = row.insertCell();
-                     const valueCell = row.insertCell();
-                     keyCell.innerHTML = property;
-                     valueCell.innerHTML = data[property];
-                 }
-             })
-     })
-}
+        // 藉此避免查詢全部所產生的分頁造成影響
+        paginationContainer.innerHTML = '';
+
+        allShowTable.innerHTML = "";
+
+        const row = allShowTable.insertRow();
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        const cell4 = row.insertCell(3);
+        const cell5 = row.insertCell(4);
+        const cell6 = row.insertCell(5);
+        const cell7 = row.insertCell(6);
+
+        cell1.innerHTML = data.pdNo;
+        cell2.innerHTML = data.pdName;
+        cell3.innerHTML = data.pdPrice;
+        cell4.innerHTML = data.pdStock;
+        cell5.innerHTML = data.pdDescription;
+        cell6.innerHTML = data.pdStatus ? '<i class="fa fa-circle text-success me-1"></i>已上架' : '<i class="fa fa-circle text-danger me-1"></i>已下架';
+        cell7.innerHTML = moment(data.pdUpdate).format('YYYY-MM-DD HH:mm:ss');
+
+        allShowTable.setAttribute('border', '1');
+      })
+  });
+});
 
 //    查詢全部
- window.onload = function() {
+window.onload = function() {
    const allshowTable = document.getElementById('allshow');
    const paginationContainer = document.querySelector('.pagination');
+   const getAllProductButton = document.getElementById('getAllProduct');
 
    const ITEMS_PER_PAGE = 10;
 
-   // 獲取所有商品數據
-   fetch(`/product/get-all`)
-     .then(response => response.json())
-     .then(data => {
-       console.log(data);
-       const recordsPerPage = ITEMS_PER_PAGE;
+   getAllProductButton.addEventListener('click', () => {
+        fetch(`/product/get-all`)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            const recordsPerPage = ITEMS_PER_PAGE;
 
-       // 分頁
-       const pageCount = Math.ceil(data.length / recordsPerPage);
-       const pagination = generatePagination(pageCount, data);
+            // 分頁
+            const pageCount = Math.ceil(data.length / recordsPerPage);
+            paginationContainer.innerHTML = ''; // 解決每按一次全部查詢底下就多一個分頁
+            const pagination = generatePagination(pageCount, data);
 
-       paginationContainer.appendChild(pagination);
+            paginationContainer.appendChild(pagination);
 
-       showPage(1, data);
-     });
+            showPage(1, data);
+          });
+      });
 
    function generatePagination(pageCount, data) {
      const paginationList = document.createElement('ul');
@@ -122,26 +138,33 @@ window.onload = function() {
      const endIndex = startIndex + ITEMS_PER_PAGE;
      const paginatedData = data.slice(startIndex, endIndex);
      for (const product of paginatedData) {
-       const row = allshowTable.querySelector('tbody').insertRow();
-       const idCell = row.insertCell();
-       const nameCell = row.insertCell();
-       const priceCell = row.insertCell();
-       const stockCell = row.insertCell();
-       const descCell = row.insertCell();
-       const statusCell = row.insertCell();
-       const updateCell = row.insertCell();
+         const row = allshowTable.querySelector('tbody').insertRow();
+         const idCell = row.insertCell();
+         const nameCell = row.insertCell();
+         const priceCell = row.insertCell();
+         const stockCell = row.insertCell();
+         const descCell = row.insertCell();
+         const statusCell = row.insertCell();
+         const updateCell = row.insertCell();
 
-       // 在表格行中顯示產品數據
-       idCell.innerHTML = product.pdNo;
-       nameCell.innerHTML = product.pdName;
-       priceCell.innerHTML = product.pdPrice;
-       stockCell.innerHTML = product.pdStock;
-       descCell.innerHTML = product.pdDescription;
-       statusCell.innerHTML = product.pdStatus;
-       updateCell.innerHTML = moment(product.pdUpdate).format('YYYY-MM-DD HH:mm:ss');
+         // 在表格行中顯示產品數據
+         idCell.innerHTML = product.pdNo;
+         nameCell.innerHTML = product.pdName;
+         priceCell.innerHTML = product.pdPrice;
+         stockCell.innerHTML = product.pdStock;
+         descCell.innerHTML = product.pdDescription;
+
+         if (product.pdStatus) {
+             statusCell.innerHTML = '<span style="margin-right: 10px;padding-left: 10px;"><i class="fa fa-circle text-success me-1"></i>已上架</span>';
+         } else {
+             statusCell.innerHTML = '<span style="margin-right: auto;padding-left: 10px;"><i class="fa fa-circle text-danger me-1"></i>已下架</span>';
+         }
+
+         updateCell.innerHTML = moment(product.pdUpdate).format('YYYY-MM-DD HH:mm:ss');
      }
    }
 }
+
 //檢查每個輸入欄位是否已填寫完
 function validateForm() {
   let pdName = document.getElementById("pdName").value;
